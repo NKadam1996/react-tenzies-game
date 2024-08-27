c81612d46ecf81d36534180806f7dcbafcd8f7c7
Nihar Kadam <nihar.kadam@gmail.com>
2024-08-28 01:49:08 +0530

    Version Change

--- src/GameOverScreen.jsx
+++ src/GameOverScreen.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function GameOverScreen(props) {
   return (
     <div className="game-over-screen">
-      <h1 className="title">Game Over!</h1>
+      <h1 className="title">Congratulations!</h1>
       <p className="score">Score: {props.rollCount}</p>
       <p className="time">Time Elapsed: {props.elapsedTime} seconds</p>
       <button className="reset-button" onClick={props.onReset}>
